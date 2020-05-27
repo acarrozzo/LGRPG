ea16e075589a4d37e068cc9959cfdc8e8844c099
Anthony Carrozzo <anthony.carrozzo@newsday.com>
2020-05-26 22:47:21 -0400

    lgchanges-world tool B

--- world-tool.php
+++ world-tool.php
@@ -33,32 +33,7 @@ error_reporting(E_ALL);
 
 // -------------------------DB CONNECT!
 require_once('db-connect.php');
-$usermain = 'root';
-$password = 'root';
-$lgdb = 'lg_db';
-$host = '127.0.0.1';
-//$host = 'localhost';
-$port = 8889;
 
-/*
-if (!empty(getenv("MYSQL_HOST"))) {
-    $usermain = getenv("MYSQL_USER");
-    $password = getenv("MYSQL_PASS");
-    $lgdb = 'lg_db';
-    $host = getenv("MYSQL_HOST");
-    //$host = 'localhost';
-    $port = 3306;
-}
-*/
-$link = $GLOBALS['link'] = mysqli_init();
-$conn = mysqli_real_connect(
-    $link,
-    $host,
-    $usermain,
-    $password,
-    $lgdb,
-    $port
-    );
 
 echo '
 <main class="worldtool">
