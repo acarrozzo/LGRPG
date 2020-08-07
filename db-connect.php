ef4555d3d8dc40618c421edcb514d7329d9da8e1
Carrozzo <acarrozz@MC02VJ08GHTD6.local>
2020-08-07 13:45:51 -0400

    lgchanges-db connect fix attempt

--- db-connect.php
+++ db-connect.php
@@ -1,11 +1,22 @@
-
 <?php
+    $usermain = 'root';
+    $password = 'root';
+    $lgdb = 'lg_db';
+    $host = '127.0.0.1';
+    //$host = 'localhost';
+    $port = 8889;
+
+
+    if (!empty(getenv("MYSQL_HOST"))) {
         $usermain = getenv("MYSQL_USER");
         $password = getenv("MYSQL_PASS");
         $lgdb = 'lg_db';
         $host = getenv("MYSQL_HOST");
         //$host = 'localhost';
         $port = 3306;
+    }
+
+
 
     $link = $GLOBALS['link'] = mysqli_init();
     $conn = mysqli_real_connect(
@@ -16,5 +27,4 @@
         $lgdb,
         $port
         );
-
-?>
+        ?>
